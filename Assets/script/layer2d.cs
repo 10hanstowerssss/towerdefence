5bad918001c1cd40b9a7ef8059b1b8fb2049a297
hiramatsuckn <nkchiramatsuryouya@gmail.com>
2017-02-10 15:07:19 +0900

    スクリプト

---
+++ Assets/script/layer2d.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class layer2d : MonoBehaviour {
+
+    int _width; // 幅
+    int _height; // 高さ
+    int _outOfRange = -1; 
+    int[] _values = null; // マップデータ
+
+    public int Width
+    {
+        get { return _width; }
+    }
+    /// 高さ
+    public int Height
+    {
+        get { return _height; }
+    }
+    public void Create(int width, int height)
+    {
+        _width = width;
+        _height = height;
+        _values = new int[Width * Height];
+    }
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
